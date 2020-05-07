4e41308674c69532c5f26c5addfe3976f66cd202
Tu Nombre <you@example.com>
2020-05-07 21:23:35 +0200

    Modificación de dichos archivos

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,6 @@ from .models import Post
 from .forms import PostForm
 
 
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
